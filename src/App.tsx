630cc3f86bd61ac687bdcf12c9d4616e5f034611
HenriSuurorg <henrijohan.suurorg@gmail.com>
2020-08-11 13:51:20 +0300

    added styled-components & created formStyledComponents.js

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,20 @@ import UserForm from "./components/userForm";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import drinksManager from "./components/drinksManager";
 import BacInformatin from "./components/bacInformation";
+import Home from "./components/home";
 
 const App: React.FC = () => {
-  return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/" exact component={UserForm} />
-        <Route path="/drinks/:id" exact component={drinksManager} />
-        <Route path="/bacinformation/:id" exact component={BacInformatin} />
-        <Route path="/" render={() => <h1>404</h1>} />
-      </Switch>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<Switch>
+				<Route path="/" exact component={Home} />
+				<Route path="/kasutaja" exact component={UserForm} />
+				<Route path="/drinks/:id" exact component={drinksManager} />
+				<Route path="/bacinformation/:id" exact component={BacInformatin} />
+				<Route path="/" render={() => <h1>404</h1>} />
+			</Switch>
+		</BrowserRouter>
+	);
 };
 
 export default App;
