cfc9e2960b08126b687bda973feaa2c0b4651e1f
Henri J. Suurorg <henri@henris-mbp.lan>
2021-09-02 14:47:04 +0200

    fixed bug where currentbac would be 0 if timepassed for the drink was 0

--- src/utils/bacCalculator.ts
+++ src/utils/bacCalculator.ts
@@ -1,5 +1,5 @@
-import { drinkType } from "../types";
-import { drinkSorter } from "./drinkSorter";
+import { drinkType } from "../types"
+import { drinkSorter } from "./drinkSorter"
 
 export const bacCalculator = ({
   widmarkFactor,
@@ -8,48 +8,50 @@ export const bacCalculator = ({
   weight,
   drinks,
 }: {
-  widmarkFactor: number;
-  absorptionRate: number;
-  eliminationRate: number;
-  weight: number;
-  drinks: drinkType[];
+  widmarkFactor: number
+  absorptionRate: number
+  eliminationRate: number
+  weight: number
+  drinks: drinkType[]
 }) => {
   // --- creates a new array where the drinks are in chronological order --- //
-  const descendingDrinks = drinkSorter(drinks, false);
+  const descendingDrinks = drinkSorter(drinks, false)
 
   // --- calculates current Blood alcohol level --- //
   let currentBAC = drinks.reduce(
     (acc: number, curDrink: drinkType, i: number) => {
-      let alcoholMass: number = 0; // Mass of alcohol in the current drink
+      curDrink.timePassed =
+        curDrink.timePassed === "0" ? "0.1" : curDrink.timePassed // in reality the time passed is never exactly 0
+      let alcoholMass: number = 0 // Mass of alcohol in the current drink
       if (curDrink.unit === "ml") {
         alcoholMass =
           parseFloat(curDrink.volume) *
           0.0007893 *
-          (parseFloat(curDrink.abv) / 100);
+          (parseFloat(curDrink.abv) / 100)
       }
 
       let x =
         alcoholMass *
-        (1 - Math.E ** (-absorptionRate * parseFloat(curDrink.timePassed)));
-      let y = widmarkFactor * weight;
+        (1 - Math.E ** (-absorptionRate * parseFloat(curDrink.timePassed)))
+      let y = widmarkFactor * weight
 
-      const curDrinkBac = (x / y) * 100;
-      let curBac = 0;
+      const curDrinkBac = (x / y) * 100
+      let curBac = 0
       if (i === descendingDrinks.length - 1) {
         curBac =
-          acc + curDrinkBac - parseFloat(curDrink.timePassed) * eliminationRate;
+          acc + curDrinkBac - parseFloat(curDrink.timePassed) * eliminationRate
       } else {
         curBac =
           acc +
           curDrinkBac -
           (parseFloat(curDrink.timePassed) -
             parseFloat(descendingDrinks[i + 1].timePassed)) *
-            eliminationRate;
+            eliminationRate
       }
 
-      return curBac;
+      return curBac
     },
     0
-  );
-  return currentBAC < 0 ? 0 : currentBAC;
-};
+  )
+  return currentBAC < 0 ? 0 : currentBAC
+}
