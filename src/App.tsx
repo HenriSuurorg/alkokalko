46a4ef9fef9cda2b3bb823aedcfd3f8aefd669ba
Henri J. Suurorg <henri@henris-mbp.lan>
2021-11-24 16:15:57 +0100

    404 test

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import React from "react";
-import Home from "./pages/Home";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
-import { User } from "./pages/User";
-import { Drinks } from "./pages/Drinks";
-import { BacInfo } from "./pages/BacInfo";
+import React from "react"
+import Home from "./pages/Home"
+import { BrowserRouter, Switch, Route } from "react-router-dom"
+import { User } from "./pages/User"
+import { Drinks } from "./pages/Drinks"
+import { BacInfo } from "./pages/BacInfo"
 
 function App() {
   return (
@@ -13,10 +13,10 @@ function App() {
         <Route path="/user" exact component={User} />
         <Route path="/user/drinks/:id" exact component={Drinks} />
         <Route path="/user/bacinfo/:id" exact component={BacInfo} />
-        <Route path="/" render={() => <h1>404</h1>} />
+        <Route path="/" render={() => <h1>404 :(</h1>} />
       </Switch>
     </BrowserRouter>
-  );
+  )
 }
 
-export default App;
+export default App
