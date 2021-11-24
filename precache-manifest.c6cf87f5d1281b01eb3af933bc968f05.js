2f39f562cc1860c4924c8a8c20518ce38be8e787
Henri J. Suurorg <henri@henris-mbp.lan>
2021-11-24 16:57:16 +0100

    Updates

---
+++ precache-manifest.c6cf87f5d1281b01eb3af933bc968f05.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad317df13dc12c8d72b40113fcec6edc",
+    "url": "/alkokalko/index.html"
+  },
+  {
+    "revision": "c8732474eff5fb35875a",
+    "url": "/alkokalko/static/css/main.79f99e9d.chunk.css"
+  },
+  {
+    "revision": "b74501121f312de2cee1",
+    "url": "/alkokalko/static/js/2.fde04783.chunk.js"
+  },
+  {
+    "revision": "32d301ecd0d2730f7ee732b0e4fe0e4b",
+    "url": "/alkokalko/static/js/2.fde04783.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8732474eff5fb35875a",
+    "url": "/alkokalko/static/js/main.deaa7a24.chunk.js"
+  },
+  {
+    "revision": "a7043478ba62eb9fd379",
+    "url": "/alkokalko/static/js/runtime-main.6887c0fa.js"
+  },
+  {
+    "revision": "9744bb8d30cadb267066b5902ce9d8b8",
+    "url": "/alkokalko/static/media/expand.9744bb8d.svg"
+  }
+]);
