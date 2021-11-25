8336ac746c82865923f6f20fb4e212cf4f7a6bdd
Henri J. Suurorg <henri@henris-mbp.lan>
2021-11-25 11:21:53 +0100

    removed redundant console.logs

--- src/utils/useLocalState.ts
+++ src/utils/useLocalState.ts
@@ -1,22 +1,20 @@
-import { useState } from "react";
+import { useState } from "react"
 
 export const useLocalStorage = (key: string, initialValue: any) => {
   const [storedValue, setStoredValue] = useState(() => {
     try {
       // Get from local storage by key
-      const item = window.localStorage.getItem(key);
+      const item = window.localStorage.getItem(key)
 
       // Parse stored json or if none return initialValue
 
-      return item ? JSON.parse(item) : initialValue;
+      return item ? JSON.parse(item) : initialValue
     } catch (error) {
       // If error also return initialValue
 
-      console.log(error);
-
-      return initialValue;
+      return initialValue
     }
-  });
+  })
 
   // Return a wrapped version of useState's setter function that ...
 
@@ -27,21 +25,19 @@ export const useLocalStorage = (key: string, initialValue: any) => {
       // Allow value to be a function so we have same API as useState
 
       const valueToStore =
-        value instanceof Function ? value(storedValue) : value;
+        value instanceof Function ? value(storedValue) : value
 
       // Save state
 
-      setStoredValue(valueToStore);
+      setStoredValue(valueToStore)
 
       // Save to local storage
 
-      window.localStorage.setItem(key, JSON.stringify(valueToStore));
+      window.localStorage.setItem(key, JSON.stringify(valueToStore))
     } catch (error) {
       // A more advanced implementation would handle the error case
-
-      console.log(error);
     }
-  };
+  }
 
-  return [storedValue, setValue];
-};
+  return [storedValue, setValue]
+}
